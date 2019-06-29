77abb55134f79e73035c997ae47fb2aea67934e0
Dmitry <gamexgroup@gmail.com>
2019-06-29 16:06:54 +0300

    add FroxNodeBase files

---
+++ Plugins/FroxPlugin/Source/FroxPlugin/Private/Nodes/FroxNodeBase.cpp
@@ -0,0 +1,58 @@
+#include "FroxNodeBase.h"
+
+const FName UFroxNodeBase::PC_Frame(TEXT("frame"));
+const FName UFroxNodeBase::PC_Property(TEXT("property"));
+
+#if WITH_EDITORONLY_DATA
+void UFroxNodeBase::PostEditChangeProperty(struct FPropertyChangedEvent& e)
+{
+	if (PropertyObserver.IsValid())
+	{
+		FName PropertyName = (e.Property != NULL) ? e.Property->GetFName() : NAME_None;
+		PropertyObserver->OnPropertyChanged(this, PropertyName);
+	}
+
+	Super::PostEditChangeProperty(e);
+}
+
+bool UFroxNodeBase::CycleInspection(uint32 UniqueID)
+{
+	for (int i = 0; i < Pins.Num(); i++)
+	{
+		if (Pins[i]->Direction == EEdGraphPinDirection::EGPD_Output)
+		{
+			for (int j = 0; j < Pins[i]->LinkedTo.Num(); j++)
+			{
+				if (Pins[i]->LinkedTo[j])
+				{
+					if (Pins[i]->LinkedTo[j]->GetOwningNode()->GetUniqueID() == UniqueID)
+					{
+						return false;
+					}
+					else
+					{
+						if (!Cast<UFroxNodeBase>(Pins[i]->LinkedTo[j]->GetOwningNode())->CycleInspection(UniqueID))
+						{
+							return false;
+						}
+					}
+				}
+			}
+		}
+	}
+	return true;
+}
+#endif //WITH_EDITORONLY_DATA
+
+void UFroxNodeBase::MarkNodeRequiresSynchronization(FString Reason, bool bRaiseGraphNeedsRecompile)
+{
+	Modify();
+	ChangeId = FGuid::NewGuid();
+
+	VisualsChangedDelegate.Broadcast(this);
+}
+
+UFroxNodeBase::FOnNodeVisualsChanged& UFroxNodeBase::OnVisualsChanged()
+{
+	return VisualsChangedDelegate;
+}
--- Plugins/FroxPlugin/Source/FroxPlugin/Public/Nodes/FroxNods.h
+++ Plugins/FroxPlugin/Source/FroxPlugin/Public/Nodes/FroxNods.h
@@ -1,74 +1,12 @@
 #pragma once
 
-#include "EdGraph/EdGraphNode.h"
-#include "OperationLogic.h"
+#include "FroxNodeBase.h"
 #include "FroxNods.generated.h"
 
 namespace frox {
 	class ComputeNode;
 	class ComputeFlow;
 } // End frox
-
-template<typename TEnum>
-static FString GetEnumValueAsString(const FString& Name, TEnum Value)
-{
-	const UEnum* enumPtr = FindObject<UEnum>(ANY_PACKAGE, *Name, true);
-	if (!enumPtr)
-	{
-		return FString("Invalid");
-	}
-
-	return enumPtr->GetEnumName((int32)Value);
-}
-
-
-/*
-UENUM(BlueprintType)
-enum class ERootType
-{
-	Type1,
-	Type2
-};
-*/
-
-class FROXPLUGIN_API FNodePropertyObserver
-{
-public:
-	virtual void OnPropertyChanged(class UEdGraphNode* Sender, const FName& PropertyName) = 0;
-};
-
-UCLASS()
-class FROXPLUGIN_API UFroxNodeBase : public UEdGraphNode
-{
-	GENERATED_BODY()
-
-public:
-	DECLARE_MULTICAST_DELEGATE_OneParam(FOnNodeVisualsChanged, UFroxNodeBase*);
-
-	static const FName PC_Frame;
-	static const FName PC_Property;
-
-#if WITH_EDITORONLY_DATA
-	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& e) override;
-	bool CycleInspection(uint32 UniqueID);
-#endif //WITH_EDITORONLY_DATA
-
-	/** Identify that this node has undergone changes that will require synchronization with a compiled script.*/
-	void MarkNodeRequiresSynchronization(FString Reason, bool bRaiseGraphNeedsRecompile);
-
-	/** Get the change id for this node. This change id is updated whenever the node is manipulated in a way that should force a recompile.*/
-	const FGuid& GetChangeId() const { return ChangeId; }
-
-	FOnNodeVisualsChanged& OnVisualsChanged();
-
-	TSharedPtr<FNodePropertyObserver> PropertyObserver;	
-
-protected:
-	UPROPERTY()
-	FGuid ChangeId;
-
-	FOnNodeVisualsChanged VisualsChangedDelegate;
-};
 
 UCLASS()
 class FROXPLUGIN_API UPropertyNode : public UFroxNodeBase

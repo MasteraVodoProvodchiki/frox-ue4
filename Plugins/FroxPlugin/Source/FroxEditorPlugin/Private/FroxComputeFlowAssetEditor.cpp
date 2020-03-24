823e9a12bd74ffad1ae0951eb7965af2b00fdb10
SergeiChernykh <sergeichernykh95@gmail.com>
2020-03-24 13:44:44 +0700

    fix include path for FAssetEditorManager

--- Plugins/FroxPlugin/Source/FroxEditorPlugin/Private/FroxComputeFlowAssetEditor.cpp
+++ Plugins/FroxPlugin/Source/FroxEditorPlugin/Private/FroxComputeFlowAssetEditor.cpp
@@ -24,6 +24,7 @@
 #include "PropertyEditorModule.h"
 #include "WorkflowOrientedApp/WorkflowTabManager.h"
 #include "Widgets/Layout/SWidgetSwitcher.h"
+#include "Toolkits/AssetEditorManager.h"
 
 #define LOCTEXT_NAMESPACE "FroxComputeFlowAssetEditor"
 
